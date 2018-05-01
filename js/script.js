4813204a4dac740feb79b0e145eb7878e742eb27
Rio Myers <riomyers@gmail.com>
2018-05-01 10:11:41 -0500

    updated to more efficient/browser friendly scrolling

--- js/script.js
+++ js/script.js
@@ -2,48 +2,25 @@ jQuery(function ($) {
     'use strict';
     (function () {
 		// Select all links with hashes
-		$('a[href*="#"]')
-		  // Ignore links that don't actually link to anything
-		  .not('[href="#"]')
-		  .not('[href="#0"]')
-		  .on('click', function(ev){
-		    // On-page links
-		    if (
-		      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
-		      && 
-		      location.hostname == this.hostname
-		    ) {
-		      // Determine element to scroll to
-		      var target = $(this.hash);
-		      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-		      // Does a scroll target exist?
-		      if (target.length) {
-		        // Only prevent default if animation is actually gonna happen
-		        ev.preventDefault();
-		        // Scroll Animation
-		        $('html, body').animate({
-		          scrollTop: target.offset().top - 20 //scroll position fix
-		        }, 737, function() {
-		          // Callback after animation
-		          // Focus pocus!
-		          var $target = $(target);
-		          $target.focus();
-		          if ($target.is(':focus')) { // Check if the target was focused
+		$('a[href*=#]:not([href=#])').click(function() {
+		    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
+		        || location.hostname == this.hostname) {
+		        var target = $(this.hash);
+		        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+		           if (target.length) {
+		             $('html,body').animate({
+		                 scrollTop: target.offset().top - 20 //scroll position fix
+		            }, 737);
 		            return false;
-		          } else {
-		            $target.attr('tabindex','-1'); // Adding tabindex for non focusable elements
-		            $target.focus(); // Reset focus
-		          };
-		        });
-		      }
+		        }
 		    }
-		  });
-		  // Youtube Popup  
-		  $('.yt').grtyoutube();
-		  //Active styling & close navbar when link is clicked
-		  $('.navbar-nav>li>a,nav a').on('click', function(){
-			$(this).addClass("active");
-		  	$('.navbar-collapse').collapse('hide');
-		  });
+		});
+		// Youtube Popup  
+		$('.yt').grtyoutube();
+		//Active styling & close navbar when link is clicked
+		$('.navbar-nav>li>a,nav a').on('click', function(){
+		$(this).addClass("active");
+			$('.navbar-collapse').collapse('hide');
+		});
     }());
 }); // JQuery end
